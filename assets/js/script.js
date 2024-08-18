427a72dced903664a36a3469baa0696852eaa808
Shihab Jamil <shihab9720@gmail.com>
2024-08-18 14:03:05 +0600

    project updated with revisions

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+AOS.init();
+$('[data-aos]').parent().addClass('hideOverflowOnMobile');
+$('.counter').counterUp();
+$(document).ready(function(){
+    $('.progress-value > span').each(function(){
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        },{
+            duration: 1500,
+            easing: 'swing',
+            step: function (now){
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
